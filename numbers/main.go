53e86eedc963dd20ea32949e4e981caf4e4d9b9d
James Salt <james.salt@container-solutions.com>
2026-05-12 15:08:28 +0100

    Add numbers example code

---
+++ numbers/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	ns := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, n := range ns {
+		if n%2 == 0 {
+			fmt.Println(n, "is even")
+		} else {
+			fmt.Println(n, "is odd")
+		}
+	}
+}
